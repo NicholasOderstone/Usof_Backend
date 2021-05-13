dd185f04be1cbe455b99b1134e932bc84fcc1997
NicholasOderstone <64272558+NicholasOderstone@users.noreply.github.com>
2021-05-13 15:54:08 +0300

    Added users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if (!(auth()->user()->role == 'admin')) {
+            return response("Access denied!", 401);
+        }
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'password' => 'required|string|confirmed',
+            'email' => 'required|string|unique:users',
+            'role' => 'required|string'
+        ]);
+
+
+
+        return User::create($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->all());
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return User::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ForgotPasswordController;
 use Illuminate\Http\Request;
@@ -20,22 +22,43 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('products', ProductController::class);
 
 // Public routes
-Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/auth/password-reset', [ForgotPasswordController::class, 'forgot']);
-Route::post('/auth/password-reset/{token}', [ForgotPasswordController::class, 'reset']);
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/password-reset', [ForgotPasswordController::class, 'forgot']);
+    Route::post('/password-reset/{token}', [ForgotPasswordController::class, 'reset']);
+});
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{id}', [PostController::class, 'show']);
 
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('', [PostController::class, 'index']);
+    Route::get('/{id}', [PostController::class, 'show']);
+});
+
+Route::group(['prefix' => 'users'], function() {
+    Route::get('', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+});
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/posts', [PostController::class, 'store']);
-    Route::patch('/posts/{id}', [PostController::class, 'update']);
-    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
-    Route::post('/auth/logout', [AuthController::class, 'logout']);
-    Route::post('/auth/change_password', [AuthController::class, 'change_password']);
+
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/change_password', [AuthController::class, 'change_password']);
+    });
+
+
+    Route::group(['prefix' => 'posts'], function () {
+        Route::post('', [PostController::class, 'store']);
+        Route::patch('/{id}', [PostController::class, 'update']);
+        Route::delete('/{id}', [PostController::class, 'destroy']);
+    }); 
+    Route::group(['prefix' => 'users'], function() {
+        Route::post('', [UserController::class, 'store']);
+        Route::patch('/{id}', [UserController::class, 'update']);
+        Route::delete('/{id}', [UserController::class, 'destroy']); 
+    }); 
 });
 
 
